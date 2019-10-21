e3058e8a2e0f294d98fc300f83fa64fb93e5c3ee
Gabriel Ramirez <gabriel@softwarenatura.com>
2019-10-20 21:13:33 -0300

    Configuration simplified by using Spring's standard Value annotation

--- src/main/java/com/example/configdemo/DemoConfig.java
+++ src/main/java/com/example/configdemo/DemoConfig.java
@@ -1,13 +1,9 @@
 package com.example.configdemo;
 
-import java.util.Objects;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
@@ -18,18 +14,11 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
 @EnableRedisHttpSession
 public class DemoConfig
 {
-	private final Environment environment;
-
-	public DemoConfig(Environment environment)
-	{
-		this.environment = environment;
-	}
-
 	@Bean
-	public RedisConnectionFactory redisConnectionFactory()
+	public RedisConnectionFactory redisConnectionFactory(
+		@Value("${redis.host}") String hostName,
+		@Value("${redis.port}") int port)
 	{
-		String hostName = environment.getProperty("redis.host");
-		int port = Integer.parseInt(Objects.requireNonNull(environment.getProperty("redis.port")));
 		RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration(hostName, port);
 		return new LettuceConnectionFactory(configuration);
 	}
